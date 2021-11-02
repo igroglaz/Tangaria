cdb22677b92426f8e6d5b08f835230db999d5d97
igroglaz <39904162+igroglaz@users.noreply.github.com>
2021-11-02 22:28:33 +0300

    Custom prices for magic ammo

--- src/server/obj-power.c
+++ src/server/obj-power.c
@@ -1174,6 +1174,12 @@ int object_value(struct player *p, const struct object *obj, int qty)
     else
         value = object_value_base(p, obj) * qty;
 
+    // Custom prices
+    if (obj->kind == lookup_kind_by_name(TV_ARROW, "Magic Arrow") ||
+        obj->kind == lookup_kind_by_name(TV_BOLT, "Magic Bolt") ||
+        obj->kind == lookup_kind_by_name(TV_SHOT, "Magic Shot"))
+        value = 230;
+
     /* Return the final value */
     return value;
 }
