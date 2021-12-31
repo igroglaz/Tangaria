0df0a478281fa72bba3334f0a12cd5da7110696c
igroglaz <39904162+igroglaz@users.noreply.github.com>
2021-12-31 12:34:05 +0300

    Assassin re

--- src/server/mon-move.c
+++ src/server/mon-move.c
@@ -2851,7 +2851,7 @@ void process_monsters(struct chunk *c, bool more_energy)
                 else if (streq(b->clazz->name, "Assassin"))
                 {
                     // Assassin class lifespan bonus
-                    if (mon->lifespan < b->lev && one_in_(2))
+                    if (mon->lifespan < b->lev)
                         mon->lifespan++;
 
                     // Purge "unconscious" sentry
