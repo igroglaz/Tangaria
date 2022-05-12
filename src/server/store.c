263c87831fe557146ca51e281aa123e314a54246
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-05-12 21:07:39 +0300

    npc_vet s

--- src/server/store.c
+++ src/server/store.c
@@ -3390,6 +3390,7 @@ void do_cmd_store(struct player *p, int pstore)
             else if (streq(s->name, "Arthur the Archer")) sound(p, MSG_NPC_ARROW);
             else if (streq(s->name, "Weapon Smiths")) sound(p, MSG_STORE_WEAPON);
             else if (streq(s->name, "Bob the villager")) sound(p, MSG_NPC_WELCOME);
+            else if (streq(s->name, "Old guard Barry")) sound(p, MSG_NPC_VET);
             break;
         case STORE_GENERAL:
             sound(p, MSG_STORE_GENERAL_SOUND);
