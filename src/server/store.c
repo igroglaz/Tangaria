432e50b20b51b9459871e8b7c9faa72c7a63150e
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-05-12 21:25:38 +0300

    npc_rough s

--- src/server/store.c
+++ src/server/store.c
@@ -3391,6 +3391,7 @@ void do_cmd_store(struct player *p, int pstore)
             else if (streq(s->name, "Weapon Smiths")) sound(p, MSG_STORE_WEAPON);
             else if (streq(s->name, "Bob the villager")) sound(p, MSG_NPC_WELCOME);
             else if (streq(s->name, "Old guard Barry")) sound(p, MSG_NPC_VET);
+            else if (streq(s->name, "Boris the Guard")) sound(p, MSG_NPC_ROUGH);
             break;
         case STORE_GENERAL:
             sound(p, MSG_STORE_GENERAL_SOUND);
