4dd72961a3f86178e15c786d63a4da6c0799bff5
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-07-27 16:45:39 +0300

    Wood-Elf race: 'y' see invisible

--- src/server/cmd-innate.c
+++ src/server/cmd-innate.c
@@ -662,6 +662,13 @@ void do_cmd_breath(struct player *p, int dir)
 
         return;
     }
+    else if (streq(p->race->name, "Wood-Elf"))
+    {
+        use_energy(p);
+        player_inc_timed(p, TMD_SINVIS, p->lev, false, false);
+        player_inc_timed(p, TMD_OCCUPIED, 2, false, false);
+        return;
+    }
     else if (streq(p->race->name, "Ent") && !streq(p->clazz->name, "Shapechanger") &&
              p->lev > 5)
     {
