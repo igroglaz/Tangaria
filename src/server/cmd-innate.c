c1befe56d402a65a35ac8ba005f8068f13d5d341
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-07-27 17:03:44 +0300

    Golem race: 'y' infravision

--- src/server/cmd-innate.c
+++ src/server/cmd-innate.c
@@ -669,6 +669,14 @@ void do_cmd_breath(struct player *p, int dir)
         player_inc_timed(p, TMD_OCCUPIED, 2, false, false);
         return;
     }
+    else if (streq(p->race->name, "Golem"))
+    {
+        use_energy(p);
+        player_inc_timed(p, TMD_SINFRA, 5 + p->lev, false, false);
+        player_dec_timed(p, TMD_FOOD, 15, false);
+        player_inc_timed(p, TMD_OCCUPIED, 2, false, false);
+        return;
+    }
     else if (streq(p->race->name, "Ent") && !streq(p->clazz->name, "Shapechanger") &&
              p->lev > 5)
     {
