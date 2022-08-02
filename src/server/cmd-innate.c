f4964cdb9fe614dc911144c54d476685330cb30c
igroglaz <39904162+igroglaz@users.noreply.github.com>
2022-08-02 23:39:55 +0300

    Dwarf race: 'y' detect treasure

--- src/server/cmd-innate.c
+++ src/server/cmd-innate.c
@@ -1073,6 +1073,15 @@ void do_cmd_breath(struct player *p, int dir)
         player_inc_timed(p, TMD_OCCUPIED, 2, true, false);
         return;
     }
+    else if (streq(p->race->name, "Dwarf"))
+    {
+        use_energy(p);
+        source_player(who, get_player_index(get_connection(p->conn)), p);
+        effect_simple(EF_DETECT_TREASURE, who, 0, 0, 0, 0, 2 + p->lev / 5, 2 + p->lev / 5, NULL);
+        player_dec_timed(p, TMD_FOOD, 5, false);
+        player_inc_timed(p, TMD_OCCUPIED, 2, true, false);
+        return;
+    }
     else if (streq(p->race->name, "Ent") && !streq(p->clazz->name, "Shapechanger") &&
              p->lev > 5)
     {
