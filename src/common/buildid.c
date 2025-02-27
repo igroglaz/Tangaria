0d226744c466ffd331823b55d4f720a498c31c49
tangar <39904162+igroglaz@users.noreply.github.com>
2025-02-27 23:10:40 +0400

    ============ ver26

--- src/common/buildid.c
+++ src/common/buildid.c
@@ -44,7 +44,7 @@ bool beta_version(void)
 #define VERSION_MINOR   6
 #define VERSION_PATCH   1
 #define VERSION_EXTRA   1
-#define VERSION_TANGARIA   25
+#define VERSION_TANGARIA   26
 
 
 // Note that it's uint16_t, so max version after << operations might be 65535.. 
@@ -66,7 +66,7 @@ uint16_t current_version(void)
 #define MIN_VERSION_MINOR   6
 #define MIN_VERSION_PATCH   1
 #define MIN_VERSION_EXTRA   1
-#define MIN_VERSION_TANGARIA   25
+#define MIN_VERSION_TANGARIA   26
 
 
 uint16_t min_version(void)
