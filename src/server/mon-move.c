0ef6f842259ec9548c68a29e756db0be53d58adc
tangar <39904162+igroglaz@users.noreply.github.com>
2025-07-22 22:14:09 +0400

    revert hydra aggro (limit circle vamp enough)

--- src/server/mon-move.c
+++ src/server/mon-move.c
@@ -2724,12 +2724,15 @@ static void monster_reduce_sleep(struct monster *mon, bool mvm)
         else if (mon_distance < 21 && streq(p->race->name, "Hydra"))
         {
             int aggro_distance = 20;
-            // Gradually reduce aggro distance from level 40 to 50
+            // Reduce aggro distance by 2 for each level starting from 40
             if (p->lev >= 40)
             {
-                // Linear reduction from 20 to 5 over 10 levels (40-50)
-                int levels_progressed = p->lev - 40;
-                aggro_distance = 20 - (levels_progressed * 15) / 10; // 15 = (20-5)
+                int level_reduction = (p->lev - 40) * 2;
+                aggro_distance = 20 - level_reduction;
+                
+                // Ensure aggro_distance doesn't go below 1
+                if (aggro_distance < 1)
+                    aggro_distance = 1;
             }
             if (mon_distance < aggro_distance)
                 monster_wake(p, mon, true, 100);
