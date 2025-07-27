6cd5c045eef9269cd3b854e40281f785051dce8e
tangar <39904162+igroglaz@users.noreply.github.com>
2025-07-27 15:58:16 +0400

    restart msg in red

--- src/server/control.c
+++ src/server/control.c
@@ -662,9 +662,9 @@ static void console_restart_warning(int ind, char *dummy)
 {
     sockbuf_t *console_buf_w = (sockbuf_t*)console_buffer(ind, CONSOLE_WRITE);
     char terminator = '\n';
-    char *warning_message = "Server restart in a few seconds.. Please come to safe place...";
+    char *warning_message = "SERVER RESTART in a few seconds.......... Please come to safe place............";
 
-    do_cmd_message(NULL, warning_message);
+    msg_all(NULL, warning_message, MSG_RED_INK);
 
     Packet_printf(console_buf_w, "%s%c", "Restart warning sent to all players", (int)terminator);
     Sockbuf_flush(console_buf_w);
